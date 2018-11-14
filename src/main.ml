00b99bf9416cb6de14f54292a4f74f99cb9581b8
TrsNium <umenyan.hy@gmail.com>
2018-11-15 00:51:13 +0900

    fix combination

--- src/main.ml
+++ src/main.ml
@@ -23,8 +23,8 @@ let documents =  read_whole_channel chan;;
 
 (*Printf.printf "%s"  documents;;*)
 
+(* TODO: surround with module*)
 type block = { _start : int; _content : string; _end : int};;
-
 let search_literals document = 
   let document_length: int = String.length document in 
 
@@ -34,7 +34,6 @@ let search_literals document =
     Printf.printf "%i%c"  start nth_char;
     *)
     if flag == true && nth_char == char then (start - 1)
-    else if flag == true && nth_char != char then find_out_duplicate_char char (start+1) false 
     else if flag == false && nth_char == char then find_out_duplicate_char char (start+1) true
     else find_out_duplicate_char char (start+1) false
   in
@@ -45,9 +44,9 @@ let search_literals document =
       let end_idx = find_out_duplicate_char '}' (start_idx+2) false  in
       let content = String.sub document (start_idx+2) (end_idx - start_idx - 2) in
       
-      (*      debug print 
+      (*      debug print *)
       Printf.printf "\n start=%i end=%i content = '%s' \n" start_idx end_idx content; 
-      *)
+      
 
       let element = { _start=start_idx; _content=content; _end=end_idx} in 
       let new_literals = literals @ [element] in
@@ -58,3 +57,11 @@ let search_literals document =
   find_out_block [] 0;;
 
 search_literals documents;;
+
+(* TODO: add evaluter module struct
+  And this module include above module as child module ? *)
+module Evaluter = struct
+  let parse_block searched_literals = 
+    Printf.printf "%s" "hoge";
+  
+end 
